9a6bb3756a660a13831cf326ef034ad7b54a23c3
fqb <fqb@fqbdeiMac.local>
2017-12-14 18:38:58 +0800

    v0.0.1

--- src/LLRedEnvelopesMgr.m
+++ src/LLRedEnvelopesMgr.m
@@ -84,25 +84,10 @@
 }
 
 - (void)successOpenRedEnvelopesNotification{
-    // 1.创建通知
     UILocalNotification *localNotification = [[UILocalNotification alloc] init];
-    // 2.设置通知的必选参数
-    // 设置通知显示的内容
     localNotification.alertBody = @"帮您领了一个大红包！快去查看吧~";
-    // 设置通知的发送时间,单位秒
-    localNotification.fireDate = [NSDate dateWithTimeIntervalSinceNow:10];
-    //解锁滑动时的事件
-    localNotification.alertAction = @"别磨蹭了!";
-    //收到通知时App icon的角标
-    //localNotification.applicationIconBadgeNumber = 1;
-    //推送是带的声音提醒，设置默认的字段为UILocalNotificationDefaultSoundName
     localNotification.soundName = UILocalNotificationDefaultSoundName;
-    // 3.发送通知(🐽 : 根据项目需要使用)
-    // 方式一: 根据通知的发送时间(fireDate)发送通知
-    //[[UIApplication sharedApplication] scheduleLocalNotification:localNotification];
-    
-    // 方式二: 立即发送通知
-     [[UIApplication sharedApplication] presentLocalNotificationNow:localNotification];
+    [[UIApplication sharedApplication] presentLocalNotificationNow:localNotification];
 }
 
 @end
